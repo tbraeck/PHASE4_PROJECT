9812bbccf5e3c32243ab9b2f714c3032e88464a6
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-15 12:48:33 -0700

    lots of changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,10 @@
 class ApplicationController < ActionController::API
   before_action :authorize
-<<<<<<< HEAD
     include ActionController::Cookies
   
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
 
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-=======
-  include ActionController::Cookies
-  
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response  
->>>>>>> new-name/Tate-Main
   
     private
   
@@ -20,17 +13,10 @@ class ApplicationController < ActionController::API
       render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
     end
 
-<<<<<<< HEAD
-  def record_not_found_response
-    render json: {errors: "model not found"}, status: :not_found
-  end
-
-=======
     def record_not_found_response
       render json: {errors: "model not found"}, status: :not_found
     end
   
->>>>>>> new-name/Tate-Main
     def render_unprocessable_entity_response(invalid)
       render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
